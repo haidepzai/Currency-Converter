b6c048bbd6d1b25db169732b4161abfae0e75906
Vu Son Hai <sv048@r133-99.pool.mintern.mi.hdm-stuttgart.de>
2018-12-19 18:58:23 +0100

    TextFileReader-class added and Currencies.txt added

---
+++ src/main/java/de/hdm_stuttgart/mi/sd1/TextFileReader.java
@@ -0,0 +1,25 @@
+package de.hdm_stuttgart.mi.sd1;
+
+import java.io.*;
+
+public class TextFileReader {
+
+    public static void main( String[] args ) throws IOException {
+
+        {
+            FileReader fr = new FileReader("Currencies.txt");
+            BufferedReader br = new BufferedReader(fr);
+
+            String zeile = "";
+
+            while( (zeile = br.readLine()) != null )
+            {
+                System.out.println(zeile);
+            }
+
+            br.close();
+
+        }
+
+    }
+}
